7f16ba98e49d4ea7ebffaa26b3eb7e0feaf9efce
Luka <luka.luka.luka93@gmail.com>
2024-09-20 12:32:02 -0700

    made a function that will speak the voice we choose from the list

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ const synth = window.speechSynthesis;
 
 // fill select box with voices
 const voiceSelect = document.querySelector('#voice-select');
+let voices;
 
 function addVoicesToSelect(){
-    const voices = synth.getVoices();
+     voices = synth.getVoices();
 
     for (let i = 0; i < voices.length; i++){
         const option = document.createElement('option');
-        option.textContent = `${voices[i].name}`;
+        option.textContent = `${voices[i].name} - ${voices[i].lang}`;
 
         if(voices[i].default) {
             option.textContent += ' - DEFAULT';
@@ -27,7 +28,20 @@ function onSubmit (e) {
     const textInput = document.querySelector('#text-input');
 
     const utterThis = new SpeechSynthesisUtterance(textInput.value);
+
+    const selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+    for(let i = 0; i < voices.length; i++){
+        if(voices[i].name === selectedOption){
+            utterThis.voice = voices[i];
+        }
+    }
+
     synth.speak(utterThis)
+}
+
+addVoicesToSelect();
+if(speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = addVoicesToSelect;
 }
 
 document.querySelector('#form').addEventListener('submit', onSubmit);
